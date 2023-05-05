720909adb6a737b12773d2186f867941dcfd2a20
victor.guidi <vsantosg4072@gmail.com>
2023-05-05 09:11:51 -0300

    We can now open a new app that will be printing the blocks of words in the app

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
 use std::fs;
+use std::thread;
 
 use slint::SharedString;
+use slint::Weak;
 
 // TODO: Add support for other file types
 // TODO: Finish implementing the GUI -> Slint.ui
@@ -21,19 +23,27 @@ impl Ebook {
         }
     }
 
-    fn iterate(&self, app: &App) {
+    fn iterate(&self, weak: Weak<App>) {
         let mut current: usize = 0;
         let mut sentence = String::new();
 
         loop {
+            let handle_weak_copy = weak.clone();
             let group = self.get_group(current);
             if group.is_empty() {
                 break;
             }
+
             sentence.push_str(&group);
+            let s = sentence.clone();
+
             println!("{}", sentence);
-            let s = SharedString::from(&sentence);
-            app.set_current_string(s);
+            _ = slint::invoke_from_event_loop(move || {
+                handle_weak_copy
+                    .unwrap()
+                    .set_current_string(SharedString::from(&s))
+            });
+
             sentence.clear();
             std::thread::sleep(std::time::Duration::from_millis(self.speed as u64));
 
@@ -66,7 +76,7 @@ slint::slint! {
     import { Button, VerticalBox } from "std-widgets.slint";
     export global ChangeText {}
     export component App inherits Window {
-        in property <string> current_string: "";
+        in property <string> current_string: "Hello, World";
         GridLayout {
             padding: 10px;
             spacing: 5px;
@@ -77,12 +87,16 @@ slint::slint! {
 
 fn main() {
     let app: App = App::new().unwrap();
+    let weak = app.as_weak();
     let args: Vec<String> = env::args().collect();
     let text_file = &args[1];
 
     let contents = fs::read_to_string(text_file).expect("Something went wrong reading the file");
-
     let ebook = Ebook::new(contents, 4, 300);
-    ebook.iterate(&app);
+
+    thread::spawn(move || {
+        ebook.iterate(weak);
+    });
+
     app.run().unwrap();
 }
