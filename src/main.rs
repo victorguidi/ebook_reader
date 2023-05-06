1560bc8ebff2abc890d9d4f50d4c60ac97ddb943
victor.guidi <vsantosg4072@gmail.com>
2023-05-06 14:10:07 -0300

    Added some style

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,7 @@ impl Ebook {
             let s = sentence.clone();
 
             println!("{}", sentence);
+
             _ = slint::invoke_from_event_loop(move || {
                 handle_weak_copy
                     .unwrap()
@@ -73,14 +74,55 @@ impl Ebook {
 }
 
 slint::slint! {
+
+    export global current_text {
+        callback populate([string]);
+    }
+
+    component side_bar inherits Rectangle {
+        width: 20%;
+        height: 100%;
+        background: #2A3424;
+    }
+
+    component main_text_area inherits Rectangle {
+        width: 80%;
+        height: 100%;
+        background: #D9D9D9;
+    }
+
     import { Button, VerticalBox } from "std-widgets.slint";
-    export global ChangeText {}
     export component App inherits Window {
-        in property <string> current_string: "Hello, World";
+
+        min_width: 500px;
+        min_height: 500px;
+
+        in property <string> current_string;
+        in property <[string]> current_text;
+
         GridLayout {
-            padding: 10px;
-            spacing: 5px;
-            Text { text: current_string; colspan: 3; }
+            width: 100%;
+            height: 100%;
+            Row {
+                side_bar {}
+                main_text_area {
+                    VerticalBox {
+                        alignment: center;
+                        padding: 10px;
+                        spacing: 5px;
+                        Text {
+                            text: current_string;
+                            color: black;
+                            wrap: word-wrap;
+                        }
+                        // for t in current_text : Text {
+                        //     text: t;
+                        //     color: t == current_string ? red : black;
+                        //     wrap: word-wrap;
+                        // }
+                    }
+                }
+            }
         }
     }
 }
@@ -92,7 +134,7 @@ fn main() {
     let text_file = &args[1];
 
     let contents = fs::read_to_string(text_file).expect("Something went wrong reading the file");
-    let ebook = Ebook::new(contents, 4, 300);
+    let ebook = Ebook::new(contents, 1, 300);
 
     thread::spawn(move || {
         ebook.iterate(weak);
